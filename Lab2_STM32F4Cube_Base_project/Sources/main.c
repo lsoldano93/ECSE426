c10a55e4581e308262f5e13ce6dad24e414a2edb
Luke <lsolda@156TR4160MP-23.campus.MCGILL.CA>
2016-02-17 15:37:17 -0500

    7 segment display code added

--- Lab2_STM32F4Cube_Base_project/Sources/main.c
+++ Lab2_STM32F4Cube_Base_project/Sources/main.c
@@ -30,14 +30,19 @@ typedef struct kalman_t{
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef ADC1_Handle;
 float	V25 = AVG_SLOPE * 25;
+int ticks = 0;
 
 /* Private function prototypes -----------------------------------------------*/
-void config_ADC_temp(void);
-void SystemClock_Config	(void);
+void ADC_Config(void);
+void SystemClock_Config(void);
+void GPIO_Config(void);
+float getTemp(void);
+void displayTemp(float temp);
+void lightDigit(int digit);
 int Kalmanfilter_asm(float* inputArray, float* outputArray, int arrayLength, kalman_t* kalman);
 
 /* Initialize -------------------------------------------------------------------*/
-void config_ADC_temp(void) {
+void ADC_Config(void) {
 
 	ADC_InitTypeDef ADC1_itd;
 	ADC_ChannelConfTypeDef ADC1_ch16;
@@ -78,53 +83,256 @@ void config_ADC_temp(void) {
 	
 	
 }
-void gpioConfig() {
-	// WARNING: DO NOT USE PORT A13, PORT A14, or PORT B3
+void GPIO_Config() {
 	
-	//PC0-PC3: Segments controls 1,2,3,4
-	//PA0: Dp
-	//PA1-PA7: A-G
+	// WARNING: DO NOT USE PORT A13, PORT A14, or PORT B3
+	/* GPIO Pin Mapping
+		 Segment Ctrls {S1:PC1, S2:PC2, S3:PC4, S4:PC5}
+		 DP Ctrl 	     {DP:PC6}
+		 Segments      {A:PB0, B:PB1, C:PB2, D:PB11, E:PB12, F:PB13, G:PB14 */
 	
 
-	GPIO_InitTypeDef GPIO_InitA, GPIO_InitC;
+	GPIO_InitTypeDef GPIO_InitB, GPIO_InitC;
 	
-	// Enable clocks for ports A & B
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	// Enable clocks for ports B & C
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
 	
-	//Initialization still needs work
-	
-	GPIO_InitA.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
-	GPIO_InitA.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitA.Pull = GPIO_PULLUP;
-	GPIO_InitA.Speed =  GPIO_SPEED_FREQ_HIGH;
+	// TODO: Initialization still needs work
+	GPIO_InitB.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14;
+	GPIO_InitB.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitB.Pull = GPIO_PULLUP;
+	GPIO_InitB.Speed =  GPIO_SPEED_FREQ_HIGH;
 	//GPIO_Init.Alternate = ;
 	
 	
-	GPIO_InitC.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3;
+	GPIO_InitC.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6;
 	GPIO_InitC.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitC.Pull = GPIO_PULLUP;
 	GPIO_InitC.Speed =  GPIO_SPEED_FREQ_HIGH;
 	//GPIO_Init.Alternate = ;
 	
-	HAL_GPIO_Init(GPIOA, &GPIO_InitA);
+	HAL_GPIO_Init(GPIOB, &GPIO_InitB);
 	HAL_GPIO_Init(GPIOC, &GPIO_InitC);
 	
 	
 }
-float getTemperature() {
+float getTemp() {
 	//HAL_StatusTypeDef HAL_ADC_PollForConversion(ADC_HandleTypeDef* hadc, uint32_t Timeout)
 
 	float VSENSE, temp;	
-	
+	HAL_ADC_Start(&ADC1_Handle);	
+
 	VSENSE = HAL_ADC_PollForConversion(&ADC1_Handle, 200);
 	HAL_ADC_GetValue(&ADC1_Handle); //gets the temperature voltage value
-	
+	HAL_ADC_Stop(&ADC1_Handle);
 	//uint32_t HAL_ADC_GetError(ADC_HandleTypeDef *hadc); //error handling
 	
 	// Temperature (in °C) = {(VSENSE – V25) / AVG_SLOPE} + 25
 	
 	return temp = ((VSENSE - V25)/AVG_SLOPE) + 25;
+}
+
+void displayTemp(float temp){
+	     
+	/* GPIO Pin Mapping
+	   Segment Ctrls {S1:PC1, S2:PC2, S3:PC4, S4:PC5}
+		 DP Ctrl 	     {DP:PC6}
+		 Segments      {A:PB0, B:PB1, C:PB2, D:PB11, E:PB12, F:PB13, G:PB14 */
+	
+	int displayValues, digit, tempValue4, tempValue3, tempValue2, tempValue1, i;
+	
+	for(i=4; i>0; i--) {
+		switch(digit) {
+			case 4:
+				//state
+				HAL_GPIO_WritePin(GPIOC, 1, GPIO_PIN_SET); //segment 4
+				break;
+			case 3:
+				//state
+				HAL_GPIO_WritePin(GPIOC, 2, GPIO_PIN_SET); //segment 3
+				break;
+			case 2:
+				//state
+				HAL_GPIO_WritePin(GPIOC, 4, GPIO_PIN_SET); //segment 2
+				break;
+			case 1:
+				//state
+				HAL_GPIO_WritePin(GPIOC,5, GPIO_PIN_SET); //segment 1
+				break;
+	
+			default:
+				break;
+		}
+		lightDigit(4);
+		
+		
+	}
+	// Determine how many values to display and what each individual value is
+//	if (temp >= 100.0){
+//		displayValues = 4;
+//		tempValue4 = temp/100;
+//		tempValue3 = temp/10 - tempValue4*10;
+//		tempValue2 = temp - tempValue4*100 - tempValue3*10;
+//		tempValue1 = (temp - tempValue4*100 - tempValue3*10 - tempValue2)*10;
+//	}
+//	else {
+//		if (temp >= 10.0){
+//				displayValues = 3;
+//				tempValue3 = temp/10;
+//				tempValue2 = temp - tempValue3*10;
+//				tempValue1 = (temp - tempValue3*10 - tempValue2)*10;
+//		}
+//		else {
+//				if (temp >= 1.0){
+//						displayValues = 2;
+//						tempValue2 = temp;
+//						tempValue1 = (temp - tempValue2)*10;
+//				}
+//				else{
+//					displayValues = 1;
+//					tempValue1 = temp*10;
+//				}
+//		}
+//	}
+		
+	// Set GPIO pins to dispaly segments
+//	for (i=4; i > 0; i--){
+//		
+//		// Display nothing
+//		if (i <= displayValues){
+//			
+//		}
+//		//Segments      {A:PB0, B:PB1, C:PB2, D:PB11, E:PB12, F:PB13, G:PB14
+//		//void HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+//		//void HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+//		
+//		
+//	}
+	
+	//segment controls
+	// Segment Ctrls {S1:PC1, S2:PC2, S3:PC4, S4:PC5}
+	
+}
+	
+
+void lightDigit(int digit) {
+	switch(digit) {
+			case 0:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_SET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //g
+				break;
+			
+			case 1:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_RESET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //g
+
+			case 2:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_RESET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_SET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g
+
+			case 3:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g
+
+			case 4:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_RESET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g			
+
+			case 5:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_RESET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g
+				break;
+			
+			case 6:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_RESET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_SET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g
+
+			case 7:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_RESET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //g
+
+			case 8:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_SET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g
+
+			case 9:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_SET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_SET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_SET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_SET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_SET); //g			
+			
+			case 10:
+				//state
+				HAL_GPIO_WritePin(GPIOA, 0, GPIO_PIN_RESET); //a
+				HAL_GPIO_WritePin(GPIOA, 1, GPIO_PIN_RESET); //b
+				HAL_GPIO_WritePin(GPIOA, 2, GPIO_PIN_RESET); //c
+				HAL_GPIO_WritePin(GPIOA, 11, GPIO_PIN_RESET); //d
+				HAL_GPIO_WritePin(GPIOA, 12, GPIO_PIN_RESET); //e
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //f
+				HAL_GPIO_WritePin(GPIOA, 13, GPIO_PIN_RESET); //g		
+				
+			default:
+				break;
+		}
 }
 
 int main(void)
@@ -140,23 +348,24 @@ int main(void)
 	
   /* Configure the system clock */
   SystemClock_Config();
-	config_ADC_temp();
+	ADC_Config();
 	
 	// Initialize ADC1 and check to make sure it happened
 	HAL_ADC_Init(&ADC1_Handle);
 	if (HAL_ADC_Init(&ADC1_Handle) != HAL_OK) Error_Handler(ADC_INIT_FAIL);
 
- 
+
 	while(1) {
 		
 		// Wait for ticks flag to be set high by SysTick
 		if (ticks){
 			
 			// Ping ADC for reading and obtain temperature
-			HAL_ADC_Start(&ADC1_Handle);	
-			temp = getTemperature();
-
+			temp = getTemp();
+			
+			displayTemp(temp);
 			printf("Tempertaure: %f \n",temp);
+			
 		
 			// Set global tick variable to 0 and wait for SysTick to reset flag
 			ticks = 0;
